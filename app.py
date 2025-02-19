652b2f58b09471edd03d2580e66c23f030b1b3fb
sond-code <67735242+sond-code@users.noreply.github.com>
2025-02-19 11:51:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,6 @@ def webhook():
     return jsonify({"status": "success"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
 
 
